a9d96fdf4e5a18e68cf38e8d8bb62cead8d7db63
Aminu Rabiu <arabiu033@gmail.com>
2023-01-19 16:03:23 +0100

    Provide the necessary validations for the user and post models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :name, presence: true, length: { in: 5..30 }
+  validates :posts_counter, presence: true, length: { in: 1..10 }, numericality: { only_integer: true }
+
   def user_posts
     Post.where('author_id = ?', id).limit(3)
   end
