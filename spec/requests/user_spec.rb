e492b0805b28d98dfd2835055ff6eff6c0aa49f1
Aminu Rabiu <arabiu033@gmail.com>
2023-01-24 12:25:08 +0100

    Add all the tests for the user requests

---
+++ spec/requests/user_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe "Users", :type => :request do
+  describe 'GET #index' do
+    before(:example) { get '/users' }  # get(:index)
+
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template(:index)
+    end
+
+    it "renders a particular content" do
+      expect(response.body).to include("Here is a list of all users")
+    end
+  end
+
+  describe 'GET #show' do
+    before(:example) { get '/users/:id' }  # get(:show)
+
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'index' template" do
+      expect(response).to render_template('show')
+    end
+
+    it "renders a particular content" do
+      expect(response.body).to include("Here is a details information about a user")
+    end
+     
+  end
+end
