1d730cfba381d9edaa740b5ecf9089894e3dbf99
Aminu Rabiu <arabiu033@gmail.com>
2023-01-26 16:03:44 +0100

    Add create and new actions in the post controlers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,28 @@ class PostsController < ApplicationController
     @num = params[:num]
     @comment = Comment.where('post_id = ?', @post.id)
   end
+
+  def new
+    @new_post = Post.new
+    @user = create_user
+  end
+
+  def create
+    @new_post = Post.new(parameters);
+    if @new_post.save
+      flash[:success] = "Post saved successfully"
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def parameters
+    para = params.require(:post).permit(:title, :text)
+    para[:comments_counter] = 0
+    para[:likes_counter] = 0
+    para[:author] = create_user
+    para
+  end
 end
