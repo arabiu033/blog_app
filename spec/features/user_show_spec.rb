8f478a3bc2bbe9775150c0099639be9b07d7291c
Aminu Rabiu <arabiu033@gmail.com>
2023-01-31 09:29:32 +0100

    Fix linters

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe 'User world', type: :system do
     subject { FactoryGirl.create(:user) }
     before do
       5.times do
-        FactoryGirl.create(:post, :author => subject)
+        FactoryGirl.create(:post, author: subject)
       end
     end
     it 'shows the right content' do
       visit user_path(subject)
-      expect(page).to have_content(subject.name.split(' ')[-1])
+      expect(page).to have_content(subject.name.split[-1])
       expect(page).to have_xpath('//img')
-      expect(page).to have_content("Number of posts: 5")
-      expect(page).to have_content("bio")
+      expect(page).to have_content('Number of posts: 5')
+      expect(page).to have_content('bio')
       expect(page).to have_content(Post.find(1).title)
       expect(page).to have_content(Post.find(2).title)
       expect(page).to have_content(Post.find(3).title)
