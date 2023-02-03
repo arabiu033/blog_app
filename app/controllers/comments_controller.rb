a791ed61a60a58514e5b57e20cd40b5aec49419b
Ahmad Zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-02-03 12:18:20 +0430

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,41 @@
 class CommentsController < ApplicationController
   load_and_authorize_resource only: :destroy
-
+​
+  def index
+    @comments = Comment.where('post_id = ?', params[:post_id])
+​
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @comments }
+    end
+  end
+​
   def new
     @comment = Comment.new
   end
-
+​
   def create
     @comment = Comment.new(parameters)
     if @comment.save
-      redirect_to user_posts_path, success: 'Comment added successfully'
+      respond_to do |format|
+        format.html { redirect_to user_posts_path, success: 'Comment added successfully' }
+        format.json { render json: @comment, status: :created }
+      end
     else
-      render :new, error: 'Error: Comment could not be saved'
+      respond_to do |format|
+        format.html { render :new, error: 'Error: Comment could not be saved' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
     end
   end
-
+​
   def destroy
     Comment.find(params[:id]).destroy
     render :new
   end
-
+​
   private
-
+​
   def parameters
     para = params.require(:comment).permit(:text)
     para[:author] = current_user
