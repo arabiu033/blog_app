6decacfb97fd06ac48661870c5f3499b0ca28bfb
Aminu Rabiu <arabiu033@gmail.com>
2023-02-03 09:58:39 +0100

    authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # add_flash_types :info, :error, :success
   protect_from_forgery with: :exception
   before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   protected
 
